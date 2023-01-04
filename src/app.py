46ea17fcdc058aff63c68a75fc0660200dfbf46d
McKenzi84 <smallbevelgear@gmail.com>
2023-01-04 22:36:40 +0100

    Msg

--- src/app.py
+++ src/app.py
@@ -11,7 +11,8 @@ wskazniki = ['Heating','HotWater','ProducedHeating','ProducedHotWater']
 
 
 
-df_read = pd.read_excel('https://github.com/McKenzi84/EyesOnTheWorld/blob/main/energia.xlsx')
+# df_read = pd.read_excel('https://github.com/McKenzi84/EyesOnTheWorld/blob/main/energia.xlsx')
+df_read = pd.read_csv('https://github.com/McKenzi84/EyesOnTheWorld/blob/main/energia.csv')
 df_read['year'] = pd.DatetimeIndex(df_read['Date']).year
 df_read['month'] = pd.DatetimeIndex(df_read['Date']).month
 df_read['day'] =pd.DatetimeIndex(df_read['Date']).day
@@ -157,4 +158,5 @@ def table_update(selected_year, selected_month, selected_metric):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host = '0.0.0.0' ,port=1112)
+    # app.run_server(debug=True, host = '0.0.0.0' ,port=1112)
+    app.run_server(debug=True,)
